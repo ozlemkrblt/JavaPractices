a802fd17050d29a6630abcfd1008b73c53e1b18e
ozlemkrblt <ozlemkarabulut99@gmail.com>
2022-09-07 17:09:38 +0300

    "KDV calculator added"

--- KDVCalculator/KDVCalculator.java
+++ KDVCalculator/KDVCalculator.java
@@ -1,23 +1,24 @@
 import java.util.Scanner;
 public class KDVCalculator {
     public static void main(String[] args) {
-        double fiyat,kdvOran= 0.18 , kdv, kdvTutar, kdvliTutar;
-    }
-        Scanner input= new Scanner(System.in);
+        double fiyat, kdvOran = 0.18, kdv, kdvTutar, kdvliTutar;
+
+        Scanner input = new Scanner(System.in);
 
         System.out.print("Ürün Fiyatını Giriniz:");
-        fiyat= input.nextDouble();
+        fiyat = input.nextDouble();
 
-        boolean cond= fiyat<=1000;
+        boolean cond = fiyat <= 1000;
 
-        kdv= cond ? 0.18 : 0.08;
-        kdvTutar=kdv*urun;
-        kdvliTutar=kdvTutar+urun;
+        kdv = cond ? 0.18 : 0.08;
+        kdvTutar = kdv * urun;
+        kdvliTutar = kdvTutar + urun;
 
-        System.out.println("Ürünün KDV'siz Fiyatı : " +fiyat);
-                System.out.println("KDV Oranı : " + kdv);
-        System.out.println("Ürünün KDV'li Fiyatı : " +kdvliTutar);
+        System.out.println("Ürünün KDV'siz Fiyatı : " + fiyat);
+        System.out.println("KDV Oranı : " + kdv);
+        System.out.println("Ürünün KDV'li Fiyatı : " + kdvliTutar);
 
         System.out.print("KDV Tutarı : " + kdvTutar);
 
     }
+}
