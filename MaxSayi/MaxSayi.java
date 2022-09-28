f2ad71caf3cfc310d1f6bcd36c6da6bd0927d26e
Özlem Karabulut <ozlemkrblt@users.noreply.github.com>
2022-09-28 17:24:04 +0300

    "MaxSayi.java added."

---
+++ MaxSayi/MaxSayi.java
@@ -0,0 +1,29 @@
+package MaxSayi;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+
+public class MaxSayi {
+    public static void main(String[] args) {
+        List list= new ArrayList<Integer>();
+
+
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("Kaç tane sayı gireceksiniz? ");
+        int n= scanner.nextInt();
+
+
+        for (int i=1; i<=n ; i++){
+            System.out.println(i + ". sayıyı giriniz: ");
+            int a=scanner.nextInt();
+            list.add(a);
+            
+        }
+        scanner.close();
+        Collections.sort(list);
+        System.out.println("En büyük sayı: " + list.get(n-1));
+        System.out.println("En küçük sayı: " + list.get(0));
+    }
+}
